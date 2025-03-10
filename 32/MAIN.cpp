d49a7179ce150830f9104d937adb24abc6371c26
Administrator <Administrator@DESKTOP-0IMDLFR>
2025-03-10 22:00:15 +0300

    Add project files.

---
+++ 32/MAIN.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm> 
+
+
+void printMax(int a, int b) {
+    std::cout << std::max(a, b) << std::endl;
+}
+
+int main() {
+    int num1, num2;
+    
+
+    std::cin >> num1 >> num2;
+
+
+    printMax(num1, num2);
+
+    return 0;
+}
